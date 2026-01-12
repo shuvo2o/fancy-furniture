0d4ad80a7d3ee4219f314814dfb9157a6724c9af
shuvo2o <eng.shuvo03@gmail.com>
2026-01-12 15:14:02 +0600

    product filtered by category

--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from './../../utils/products';
 import ProductCard from './ProductCard';
 
 const Products = ({ headline }) => {
   const categories = ["Chair", "Beds", "Sofa", "Lamp"];
+  const [selectedCategory, setSelectedCategory] = useState("Chair");
+  const filteredProducts = products.filter((product)=> product.category === selectedCategory)
+
   return (
     <div>
       <div className='section-container'>
@@ -13,8 +16,11 @@ const Products = ({ headline }) => {
           <div className='flex flex-col items-center justify-center gap-4 sm:flex-row md:justify-between'>
             {categories.map((category) => (
               <button
+              onClick={()=>{
+                setSelectedCategory(category)
+              }}
                 key={category}
-                className={`py-1.5 sm:px-5 px-8 rounded-full  hover:bg-amber-500 hover:text-white transition-colors`}
+                className={`py-1.5 sm:px-5 px-8 rounded-full  hover:bg-amber-500 hover:text-white transition-colors ${selectedCategory === category ? 'bg-amber-500 text-white' : 'bg-white text-black'}`}
               >
                 {category}
               </button>
@@ -24,8 +30,8 @@ const Products = ({ headline }) => {
         {/* products grid */}
         <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4'>
           {
-            products.map((product, index)=>(
-              <ProductCard key={index} product={product}/>
+            filteredProducts.map((product, index) => (
+              <ProductCard key={index} product={product} />
             ))
           }
         </div>
